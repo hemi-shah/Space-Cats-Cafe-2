d51248bede6d95a4671ce8d02185fe0c8be4ae72
mayafederico <maya.federico8@gmail.com>
2025-11-10 02:05:53 -0800

    Worked on a bit more implementation
    can hardcode a rating right now in DrinkVerifier

--- src/UnitySpaceCats2/Assets/Game/Runtime/Scripts/DrinkVerifier.cs
+++ src/UnitySpaceCats2/Assets/Game/Runtime/Scripts/DrinkVerifier.cs
@@ -6,6 +6,8 @@ public class DrinkVerifier : ObserverMonoBehaviour
 {
 
     public OrderManager OrderManager;
+
+    public int lastRating { get; private set; }
     
     
     protected override void Subscribe()
@@ -25,5 +27,56 @@ public class DrinkVerifier : ObserverMonoBehaviour
         // compare the drink and the order
         
         // call some method to provide a rating
+        // changes lastRating based on accuracy of drink
+        
+        
+        
+        // 7 total points
+        // optional: 0 points: remake drink (?)
+        // 0 - 1 points: 1 star
+        // 2 - 3 points: 2 stars
+        // 4 - 5 points: 3 stars
+        // 6 points: 4 stars
+        // 7 points: 5 stars
+
+        int points;
+        
+        // if drink isHot matches orderTicket isHot, +1
+        // if iceCubes is correct, +1
+        // if syrupType is correct, +1
+        // if milkType is correct, +1
+        // if whipped cream is correct, +1
+        // if chocolate drizzle is correct, +1
+        // if caramel drizzle is correct, +1
+        
+        points = 7; // placeholder point value
+
+        // set rating
+        if (points == 0 || points == 1)
+        {
+            lastRating = 1;
+        }
+        else if (points == 2 || points == 3)
+        {
+            lastRating = 2;
+        }
+        else if (points == 4 || points == 5)
+        {
+            lastRating = 3;
+        }
+        else if (points == 6)
+        {
+            lastRating = 4;
+        }
+        else if (points == 7)
+        {
+            lastRating = 5;
+        }
+        else
+        {
+            Debug.LogError("points not 0-7");
+        }
+
     }
+    
 }
