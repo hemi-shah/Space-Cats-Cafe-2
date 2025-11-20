2ae89385ae255d660a9b645e5ece0d49b679a49b
hemi-shah <heshah@chapman.edu>
2025-11-20 14:41:33 -0800

    Moved tests from Shared to Tests

--- src/Shared/Game399.Tests/DrinkServiceTests.cs
+++ src/Shared/Game399.Tests/DrinkServiceTests.cs
@@ -1,45 +1,45 @@
-using NUnit.Framework;
-using Game399.Shared.Services;
-using Game399.Shared.Enums;
-using System.Collections.Generic;
-
-namespace Game399.Tests
-{
-    public class DrinkServiceTests
-    {
-        private class TestDrinkService : DrinkService
-        {
-            public List<MockDrink> Drinks { get; } = new();
-
-            public TestDrinkService() : base(EmptyGameLog.Instance) { }
-
-            // Override CanMakeDrink to use MockDrink for testing
-            public new bool CanMakeDrink(DrinkComponent component = DrinkComponent.None)
-            {
-                // simple mock logic for test purposes
-                if (component == DrinkComponent.None)
-                    return Drinks.Count > 0;
-                return true;
-            }
-
-            // Optionally, add a method to add mock drinks
-            public void AddMockDrink(MockDrink drink) => Drinks.Add(drink);
-        }
-
-        [Test]
-        public void CanMakeDrinkTests_FailOnEmptyInput()
-        {
-            var drinkService = new TestDrinkService();
-            Assert.That(drinkService.CanMakeDrink(), Is.False);
-        }
-
-        [Test]
-        public void CanMakeDrinkTests_Success()
-        {
-            var drinkService = new TestDrinkService();
-            drinkService.AddMockDrink(new MockDrink());
-
-            Assert.That(drinkService.CanMakeDrink(DrinkComponent.Milk), Is.True);
-        }
-    }
-}
+// using NUnit.Framework;
+// using Game399.Shared.Services;
+// using Game399.Shared.Enums;
+// using System.Collections.Generic;
+//
+// namespace Game399.Tests
+// {
+//     public class DrinkServiceTests
+//     {
+//         private class TestDrinkService : DrinkService
+//         {
+//             public List<MockDrink> Drinks { get; } = new();
+//
+//             public TestDrinkService() : base(EmptyGameLog.Instance) { }
+//
+//             // Override CanMakeDrink to use MockDrink for testing
+//             public new bool CanMakeDrink(DrinkComponent component = DrinkComponent.None)
+//             {
+//                 // simple mock logic for test purposes
+//                 if (component == DrinkComponent.None)
+//                     return Drinks.Count > 0;
+//                 return true;
+//             }
+//
+//             // Optionally, add a method to add mock drinks
+//             public void AddMockDrink(MockDrink drink) => Drinks.Add(drink);
+//         }
+//
+//         [Test]
+//         public void CanMakeDrinkTests_FailOnEmptyInput()
+//         {
+//             var drinkService = new TestDrinkService();
+//             Assert.That(drinkService.CanMakeDrink(), Is.False);
+//         }
+//
+//         [Test]
+//         public void CanMakeDrinkTests_Success()
+//         {
+//             var drinkService = new TestDrinkService();
+//             drinkService.AddMockDrink(new MockDrink());
+//
+//             Assert.That(drinkService.CanMakeDrink(DrinkComponent.Milk), Is.True);
+//         }
+//     }
+// }
--- src/Shared/Game399.Tests/MockDrink.cs
+++ src/Shared/Game399.Tests/MockDrink.cs
@@ -1,28 +1,28 @@
-public class MockDrink
-{
-    public Temperature Temp { get; set; }
-    public CoffeeType Coffee { get; set; }
-    public MilkType Milk { get; set; }
-    public int IceLevel { get; set; } = 0;
-    public Dictionary<string,int> SyrupCounts { get; set; } = new();
-    public HashSet<string> Toppings { get; set; } = new();
-
-    public void AddMilk(MilkType milk) => Milk = milk;
-    public void AddSyrup(string syrup)
-    {
-        if (!SyrupCounts.ContainsKey(syrup)) SyrupCounts[syrup] = 0;
-        SyrupCounts[syrup]++;
-    }
-    public void AddTopping(string topping) => Toppings.Add(topping);
-
-    public string GetSpriteName()
-    {
-        if (Temp == Temperature.Hot)
-            return Coffee == CoffeeType.Black ? "HotDrinkCoffeeSprite" : "HotDrinkMilkSprite";
-        if (IceLevel == 0 || IceLevel > 4)
-            return $"IcedDrinkEmpty{Coffee}Sprite";
-        return $"IcedDrink{NumberToWord(IceLevel)}Ice{Coffee}Sprite";
-    }
-
-    private string NumberToWord(int number) => number switch {1=>"One",2=>"Two",3=>"Three",4=>"Four", _=>""};
-}
+// public class MockDrink
+// {
+//     public Temperature Temp { get; set; }
+//     public CoffeeType Coffee { get; set; }
+//     public MilkType Milk { get; set; }
+//     public int IceLevel { get; set; } = 0;
+//     public Dictionary<string,int> SyrupCounts { get; set; } = new();
+//     public HashSet<string> Toppings { get; set; } = new();
+//
+//     public void AddMilk(MilkType milk) => Milk = milk;
+//     public void AddSyrup(string syrup)
+//     {
+//         if (!SyrupCounts.ContainsKey(syrup)) SyrupCounts[syrup] = 0;
+//         SyrupCounts[syrup]++;
+//     }
+//     public void AddTopping(string topping) => Toppings.Add(topping);
+//
+//     public string GetSpriteName()
+//     {
+//         if (Temp == Temperature.Hot)
+//             return Coffee == CoffeeType.Black ? "HotDrinkCoffeeSprite" : "HotDrinkMilkSprite";
+//         if (IceLevel == 0 || IceLevel > 4)
+//             return $"IcedDrinkEmpty{Coffee}Sprite";
+//         return $"IcedDrink{NumberToWord(IceLevel)}Ice{Coffee}Sprite";
+//     }
+//
+//     private string NumberToWord(int number) => number switch {1=>"One",2=>"Two",3=>"Three",4=>"Four", _=>""};
+// }
