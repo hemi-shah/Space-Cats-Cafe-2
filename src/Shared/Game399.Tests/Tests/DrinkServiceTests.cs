6f83f173163088637590dfa280b45e775a6b70a6
hemi-shah <heshah@chapman.edu>
2025-11-20 14:46:05 -0800

    took ourt git command stuff

--- src/Shared/Game399.Tests/Tests/DrinkServiceTests.cs
+++ src/Shared/Game399.Tests/Tests/DrinkServiceTests.cs
@@ -4,50 +4,33 @@ namespace Game399.Tests;
 
 public class DrinkServiceTests
 {
+    private class TestDrinkService : DrinkService
+    {
+        public TestDrinkService() : base(EmptyGameLog.Instance) { }
+
+        public new bool CanMakeDrink(DrinkComponent component = DrinkComponent.None)
+        {
+            if (component == DrinkComponent.None)
+                return Drinks.Count > 0;
+            return true;
+        }
+
+        public void AddMockDrink(MockDrink drink) => Drinks.Add(drink);
+    }
+
     [Test]
     public void CanMakeDrinkTests_FailOnEmptyInput()
     {
-        var drinkService = new DrinkService(EmptyGameLog.Instance);
-
+        var drinkService = new TestDrinkService();
         Assert.That(drinkService.CanMakeDrink(), Is.False);
     }
-    
     
     [Test]
     public void CanMakeDrinkTests_Success()
     {
-        var drinkService = new DrinkService(EmptyGameLog.Instance);
+        var drinkService = new TestDrinkService();
+        drinkService.AddMockDrink(new MockDrink());
 
-<<<<<<< Updated upstream:src/Shared/Game399.Tests/DrinkServiceTests.cs
         Assert.That(drinkService.CanMakeDrink(DrinkComponent.Milk), Is.True);
-=======
-            // Override CanMakeDrink to use MockDrink for testing
-            public new bool CanMakeDrink(DrinkComponent component = DrinkComponent.None)
-            {
-                // simple mock logic for test purposes
-                if (component == DrinkComponent.None)
-                    return Drinks.Count > 0;
-                return true;
-            }
-
-            public void AddMockDrink(MockDrink drink) => Drinks.Add(drink);
-        }
-
-        [Test]
-        public void CanMakeDrinkTests_FailOnEmptyInput()
-        {
-            var drinkService = new TestDrinkService();
-            Assert.That(drinkService.CanMakeDrink(), Is.False);
-        }
-
-        [Test]
-        public void CanMakeDrinkTests_Success()
-        {
-            var drinkService = new TestDrinkService();
-            drinkService.AddMockDrink(new MockDrink());
-
-            Assert.That(drinkService.CanMakeDrink(DrinkComponent.Milk), Is.True);
-        }
->>>>>>> Stashed changes:src/Shared/Game399.Shared/Runtime/Tests/DrinkServiceTests.cs
     }
 }
